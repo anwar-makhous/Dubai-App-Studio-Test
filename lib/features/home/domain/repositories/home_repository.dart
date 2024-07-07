a759955051db73f3d2299e9d16ba6f8add94f498
Anwar Makhous <anwar.makhous@gmail.com>
2024-07-07 13:58:56 +0530

    refactor home related files

--- lib/features/home/domain/repositories/home_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:dubai_app_studio/core/error/error.dart';
-import 'package:dubai_app_studio/features/home/domain/entities/balance.dart';
-
-abstract class HomeRepository {
-  Future<Either<Failure, Balance>> getBalance();
-}
